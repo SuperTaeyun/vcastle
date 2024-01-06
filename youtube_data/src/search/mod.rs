88bf4996bf2b04d76f7e50c3434ff704fc5039d7
SuperTaeyun <super.kim.taeyun@gmail.com>
2024-01-06 16:15:24 +0900

    Add: Video, SearchApi/List 타입 추가

--- youtube_data/src/search/mod.rs
+++ youtube_data/src/search/mod.rs
@@ -1,1 +1,27 @@
+use reqwest::Client;
+
 pub mod model;
+
+pub struct SearchApi {
+    api_key: String,
+    client: Client,
+}
+
+impl SearchApi {
+    pub fn new(api_key: String, client: Client) -> Self {
+        Self { api_key, client }
+    }
+
+    pub fn list(&self) -> SearchList {
+        todo!()
+    }
+}
+
+pub struct SearchList {}
+
+impl SearchList {
+    pub fn request() {}
+}
+
+#[cfg(test)]
+mod tests {}
--- youtube_data/src/videos/mod.rs
+++ youtube_data/src/videos/mod.rs
@@ -1,1 +1,27 @@
+use reqwest::Client;
+
 pub mod model;
+
+pub struct VideoApi {
+    api_key: String,
+    client: Client,
+}
+
+impl VideoApi {
+    pub fn new(api_key: String, client: Client) -> Self {
+        Self { api_key, client }
+    }
+
+    pub fn list(&self) -> VideoList {
+        todo!()
+    }
+}
+
+pub struct VideoList {}
+
+impl VideoList {
+    pub fn request() {}
+}
+
+#[cfg(test)]
+mod tests {}
